c069c13dc2d6c926b73427d9fcebf4ff730b3345
broberson7801 <broberson7801@gmail.com>
2017-06-26 17:26:13 -0600

    EntireDateTimePresentation

---
+++ src/dateTime/DateTimeTestQuestions.java
@@ -0,0 +1,88 @@
+package dateTime;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class DateTimeTestQuestions {
+
+	public static void main(String[] args) {
+		DateTimeTestQuestions questions = new DateTimeTestQuestions();
+
+		questions.commonSense();
+		questions.period();
+		questions.format();
+		questions.parse();
+		questions.dateTimeAndPeriodTogether(2);
+		questions.duration();
+
+	}
+
+	public void commonSense() {
+
+		// Will the following code compile and/or run?
+		// LocalDateTime dateTime = new LocalDateTime();
+
+		// What is the output of the following code?
+		// LocalDate date = LocalDate.of(2020, Month.JANUARY, 20);
+		// date.plusDays(10);
+		// System.out.println(date);
+
+		// What is wrong with the code below, if anything?
+		// LocalDate theDate = LocalDate.of(2020, Month.JANUARY, 20);
+		// theDate = theDate.plusMinutes(1);
+		// System.out.println(theDate);
+
+		// What is the result of the following code?
+		// System.out.println(LocalDate.of(2015, Month.JANUARY, 6_908_977));
+
+	}
+
+	public void period() {
+		// What is the output of the following code?
+		// Period thePeriod = Period.ofYears(1).ofWeeks(3).ofDays(9);
+		// System.out.println(thePeriod);
+
+	}
+
+	public void format() {
+		// LocalDateTime getDrunkAndKickTheDogDate = LocalDateTime.of(2017,
+		// Month.JUNE, 28, 8, 57, 19, 160);
+		// System.out.println(getDrunkAndKickTheDogDate);
+		//
+		// DateTimeFormatter f = DateTimeFormatter.ofPattern("MMM dd, YY,
+		// hh:mm");
+		// System.out.println(getDrunkAndKickTheDogDate.format(f));
+
+		// What will Print?
+		// System.out.println(f);
+	}
+
+	public void parse() {
+		// DateTimeFormatter f = DateTimeFormatter.ofPattern("MM dd yyyy");
+		// LocalDate date = LocalDate.parse("01 02 2015", f);
+		// LocalTime time = LocalTime.parse("11:22");
+		// System.out.println(date);
+		// System.out.println(time);
+	}
+
+	public void dateTimeAndPeriodTogether(int numDays) {
+		// LocalDate date = LocalDate.now();
+		// Period days = Period.ofDays(numDays);
+		// Period months = Period.ofMonths(2);
+		// System.out.println(date.plus(days));
+		// System.out.println(date.minus(months));
+		//
+	}
+
+	public void duration() {
+
+		Duration timeUntilWifeComesHome;
+
+		LocalDateTime partyTime = LocalDateTime.of(2017, Month.JUNE, 28, 21, 30, 40, 490);
+		LocalDateTime partyOver = LocalDateTime.of(2017, Month.JUNE, 29, 02, 30, 25, 190);
+
+		timeUntilWifeComesHome = Duration.between(partyOver, partyTime);
+		System.out.println(timeUntilWifeComesHome);
+	}
+}
